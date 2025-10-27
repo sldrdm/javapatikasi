9dd79310f1e90317cbe833a3b32b0558f62915c5
selda erdem <e.selda.erdem@outlook.com>
2025-10-27 23:48:17 +0300

    feat:Hava Sıcaklığına göre etkinlik seçimi

--- src/HavaDurumunaGoreEtkinlik.java
+++ src/HavaDurumunaGoreEtkinlik.java
@@ -10,13 +10,13 @@ public class HavaDurumunaGoreEtkinlik {
             System.out.println("Kayak tatiline gidebilirsiniz.");
         }
         else if (sicaklik > 5 && sicaklik <= 15) {
-            System.out.println("sinemaya gidebilirisiniz");
+            System.out.println("sinemaya gidebilirisiniz.");
         }
         else if (sicaklik > 15 && sicaklik <= 25) {
-            System.out.println("pikniğe gidebilirisiniz");
+            System.out.println("pikniğe gidebilirisiniz.");
         }
         else if (sicaklik>25 && sicaklik<=35) {
-            System.out.println("yüzmeye gidebilirisiniz");
+            System.out.println("yüzmeye gidebilirisiniz.");
         }
 
     }

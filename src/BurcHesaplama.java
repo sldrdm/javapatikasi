98972a601db20bdadcb456b0482253811b155580
selda erdem <e.selda.erdem@outlook.com>
2025-11-10 01:55:55 +0300

    PolindromSayisi alıştırması 1456-6541 eşit mi?

---
+++ src/BurcHesaplama.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BurcHesaplama {
+    static void main (String[] args){
+        int dogumtarihi;
+        Scanner inp=new Scanner(System.in);
+        System.out.println("Doğum tarihinizi girin");
+        dogumtarihi=inp.nextInt();
+    }
+}
---
+++ src/WhileDongu.java
@@ -0,0 +1,17 @@
+public class WhileDongu {
+    public static void main (String[] args){
+        int i=1;
+        int k;
+        while(i<5){
+
+            System.out.println("i : "+i);
+            k=1;
+            while(k<=10){
+                System.out.print( " K:"+k);
+                k++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
